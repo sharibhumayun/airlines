5d11659895352f094ce9701f2a545eefdab9d5da
sharibhumayun <66083313+sharibhumayun@users.noreply.github.com>
2024-05-30 00:31:30 +0300

    Add files via upload

--- app_py.py
+++ app_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1fQzo4--qVABFSZ-cRHjPnxXF-7NdOOX9
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import sklearn
